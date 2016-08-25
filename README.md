5a65469d915b14ef34a97b3c6c9efd4d1bbf2fa8
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-08-25 15:02:15 -0500

    nomas un stub pero con carnita

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # tiangCLis
+
 Tianguis virtual, comercio solidario, web en common lisp.
+
+Hay un diseño
+[acá](https://github.com/rgarcia-herrera/tianguis). Veamos qué tal es
+implementarlo con tecnología alienígena.
+
+# Instalar lo de Lisp
+
+Hay que instalar QuickLisp. Hay que hacer que QuickLisp cargue
+automáticamente. El resto se resuelve solo.
+
+Para hackearle se recomienda emacs con slime y sbcl.
+
+
+# Instalar Semantic UI
+
+[Semantic](http://semantic-ui.com) usamos para la web.
+
+    npm install -g gulp
+    npm install semantic-ui --save
+    cd semantic
+    gulp build
---
+++ tianguis.lisp
@@ -0,0 +1,82 @@
+(ql:quickload "cl-who")
+(ql:quickload "hunchentoot")
+(ql:quickload "parenscript")
+(ql:quickload "elephant")
+
+(in-package :cl-user)
+
+(defpackage :tianguis
+  (:use :cl :cl-who :hunchentoot :parenscript :elephant))
+
+(in-package :tianguis)
+
+;; Start our web server.
+; (setf *web-server* (start-server :port 8080))
+(setf *web-server*
+      (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4243)))
+; 
+
+(setq *dispatch-table*
+ (list
+  (create-regex-dispatcher "^/" 'oferta)
+  (create-regex-dispatcher "^/publicar/oferta" 'vote)))
+
+
+;; Publish all static content.
+(push (create-static-file-dispatcher-and-handler "/logo.jpg" "imgs/logo.jpg") *dispatch-table*)
+(push (create-static-file-dispatcher-and-handler "/semantic/dist/semantic.min.css" "semantic/dist/semantic.min.css") *dispatch-table*)
+(push (create-static-file-dispatcher-and-handler "/semantic/dist/semantic.min.js" "semantic/dist/semantic.min.js") *dispatch-table*)
+
+
+;; Launch Elephant
+;; (elephant:open-store '(:BDB "/tmp/game.db"))
+(elephant:open-store '(:CLSQL (:SQLITE3 "/tmp/tianguis.sqlite")))
+
+
+;; Represent each game as an instance of the Elephant persistant class.
+(defpclass persistent-merchandise ()
+  ((name :reader name :initarg :name :index t)
+   (quantity :accessor quantity :initarg :votes :initform 0 :index t)))
+
+
+
+
+;; All pages on the Retro Games site will use the following macro; less to type and 
+;; a uniform look of the pages (defines the header and the stylesheet).
+(defmacro standard-page ((&key title) &body body)
+  `(with-html-output-to-string (*standard-output* nil :prologue t :indent t)
+     (:html :xmlns "http://www.w3.org/1999/xhtml"  :xml\:lang "en" :lang "en"
+       (:head 
+         (:meta :http-equiv "Content-Type" :content "text/html;charset=utf-8")
+	 (:title ,title)
+	 (:link :type "text/css" :rel "stylesheet" :href "/semantic/dist/semantic.min.css")
+	 (:script :src "/semantic/dist/semantic.min.js"))
+       (:body
+	(:br)
+	(:div :class "ui container" 
+	      (:h2 :class "ui center dividing header" ,title)
+	      (:div :class "ui container"
+		    ,@body))))))
+
+;;
+;; The functions responsible for generating the HTML go here.
+;;
+
+(defun oferta ()
+  (standard-page (:title "Tianguis")
+    (:p "La Granja ofrece, en su centro de distribución")
+    (:table :class "ui right aligned table"
+	    (:thead
+	     (:tr
+	      (:th :class "left aligned" "producto")
+	      (:th "cantidad")
+	      (:th "precio")))
+	    (:tbody
+	     (:tr
+	      (:td "acelga hidropónica")
+	      (:td "2")
+	      (:td "300"))))))
+
+
+
+
